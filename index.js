0dc03a624ee06d21db3f38335604b260aa29fb8a
Georgi Petrushev <figoto660@gmail.com>
2019-08-22 01:30:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ function whisper(string) {
   return string.toLowerCase();
 }
 function logShout(string){
-  var upCase = string.toUpperCase();
-  consol.log(upCase);
+  
+  consol.log(string.toUpperCase());
 }
